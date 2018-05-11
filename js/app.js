ca5a41edce2081f7c57fe2bf2c0c1ca336b2dff7
Nick Mychka <nickmychka@gmail.com>
2018-05-11 17:29:23 +0300

    added a big play button

--- js/app.js
+++ js/app.js
@@ -24,3 +24,35 @@ var app = new Framework7({
 		placementId: 'pltd4o7ibb9rc653x14',
 	},
 });
+
+var $$ = Framework7.$;
+$$(document).on('pagecreate', function () {
+	//do something
+
+}).on('click', '.play-btn', function (e) {
+
+		var $audio = $("#audio");
+				
+
+		/*................. play/pause................ */
+		
+		if($audio[0].paused) {
+			$audio[0].play();
+		} else {
+			$audio[0].pause();
+		}
+
+		$(this).toggleClass('pause-btn');
+
+
+		$audio.on('timeupdate', function() {
+			if($audio[0].ended) { 
+				jQuery('.play-btn').removeClass('pause-btn');
+			}
+
+		});
+
+	});
+
+
+ 
