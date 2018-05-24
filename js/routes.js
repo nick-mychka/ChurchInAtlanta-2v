6aba0bbe1bdb97087c5bec8afa499d601dd2ef84
Ihor92 <salfetnikov92@gmail.com>
2018-05-24 14:38:27 +0300

    Added three season

--- js/routes.js
+++ js/routes.js
@@ -151,6 +151,169 @@ var routes = [
 		url: './pages/2013/fall-2013/messages/msg-6-fall_13.html',
 		name: 'msg-6-fall_13',
 	},
+
+	//fall 2012
+	{
+		path: '/msg-1-fall_12/',
+		url: './pages/2012/fall-2012/messages/msg-1-fall_12.html',
+		name: 'msg-1-fall_12',
+	},
+	{
+		path: '/msg-2-fall_12/',
+		url: './pages/2012/fall-2012/messages/msg-2-fall_12.html',
+		name: 'msg-2-fall_12',
+	},
+	{
+		path: '/msg-3-fall_12/',
+		url: './pages/2012/fall-2012/messages/msg-3-fall_12.html',
+		name: 'msg-3-fall_12',
+	},
+	{
+		path: '/msg-4-fall_12/',
+		url: './pages/2012/fall-2012/messages/msg-4-fall_12.html',
+		name: 'msg-4-fall_12',
+	},
+	{
+		path: '/msg-5-fall_12/',
+		url: './pages/2012/fall-2012/messages/msg-5-fall_12.html',
+		name: 'msg-5-fall_12',
+	},
+	{
+		path: '/msg-6-fall_12/',
+		url: './pages/2012/fall-2012/messages/msg-6-fall_12.html',
+		name: 'msg-6-fall_12',
+	},
+
+	//spr 2012
+	{
+		path: '/msg-1-spr_12/',
+		url: './pages/2012/spr-2012/messages/msg-1-spr_12.html',
+		name: 'msg-1-spr_12',
+	},
+	{
+		path: '/msg-2-spr_12/',
+		url: './pages/2012/spr-2012/messages/msg-2-spr_12.html',
+		name: 'msg-2-spr_12',
+	},
+	{
+		path: '/msg-3-spr_12/',
+		url: './pages/2012/spr-2012/messages/msg-3-spr_12.html',
+		name: 'msg-3-spr_12',
+	},
+	{
+		path: '/msg-4-spr_12/',
+		url: './pages/2012/spr-2012/messages/msg-4-spr_12.html',
+		name: 'msg-4-spr_12',
+	},
+
+	//fall 2011
+	{
+		path: '/msg-1-fall_11/',
+		url: './pages/2011/fall-2011/messages/msg-1-fall_11.html',
+		name: 'msg-1-fall_11',
+	},
+	{
+		path: '/msg-2-fall_11/',
+		url: './pages/2011/fall-2011/messages/msg-2-fall_11.html',
+		name: 'msg-2-fall_11',
+	},
+	{
+		path: '/msg-3-fall_11/',
+		url: './pages/2011/fall-2011/messages/msg-3-fall_11.html',
+		name: 'msg-3-fall_11',
+	},
+	{
+		path: '/msg-4-fall_11/',
+		url: './pages/2011/fall-2011/messages/msg-4-fall_11.html',
+		name: 'msg-4-fall_11',
+	},
+	{
+		path: '/msg-5-fall_11/',
+		url: './pages/2011/fall-2011/messages/msg-5-fall_11.html',
+		name: 'msg-5-fall_11',
+	},
+	{
+		path: '/msg-6-fall_11/',
+		url: './pages/2011/fall-2011/messages/msg-6-fall_11.html',
+		name: 'msg-6-fall_11',
+	},
+
+	//spr 2011
+	{
+		path: '/msg-1-spr_11/',
+		url: './pages/2011/spr-2011/messages/msg-1-spr_11.html',
+		name: 'msg-1-spr_11',
+	},
+	{
+		path: '/msg-2-spr_11/',
+		url: './pages/2011/spr-2011/messages/msg-2-spr_11.html',
+		name: 'msg-2-spr_11',
+	},
+	{
+		path: '/msg-3-spr_11/',
+		url: './pages/2011/spr-2011/messages/msg-3-spr_11.html',
+		name: 'msg-3-spr_11',
+	},
+	{
+		path: '/msg-4-spr_11/',
+		url: './pages/2011/spr-2011/messages/msg-4-spr_11.html',
+		name: 'msg-4-spr_11',
+	},
+	//fall 2010
+	{
+		path: '/msg-1-fall_10/',
+		url: './pages/2010/fall-2010/messages/msg-1-fall_10.html',
+		name: 'msg-1-fall_10',
+	},
+	{
+		path: '/msg-2-fall_10/',
+		url: './pages/2010/fall-2010/messages/msg-2-fall_10.html',
+		name: 'msg-2-fall_10',
+	},
+	{
+		path: '/msg-3-fall_10/',
+		url: './pages/2010/fall-2010/messages/msg-3-fall_10.html',
+		name: 'msg-3-fall_10',
+	},
+	{
+		path: '/msg-4-fall_10/',
+		url: './pages/2010/fall-2010/messages/msg-4-fall_10.html',
+		name: 'msg-4-fall_10',
+	},
+	{
+		path: '/msg-5-fall_10/',
+		url: './pages/2010/fall-2010/messages/msg-5-fall_10.html',
+		name: 'msg-5-fall_10',
+	},
+	{
+		path: '/msg-6-fall_10/',
+		url: './pages/2010/fall-2010/messages/msg-6-fall_10.html',
+		name: 'msg-6-fall_10',
+	},
+	//spr 2010
+	{
+		path: '/msg-1-spr_10/',
+		url: './pages/2010/spr-2010/messages/msg-1-spr_10.html',
+		name: 'msg-1-spr_10',
+	},
+	{
+		path: '/msg-2-spr_10/',
+		url: './pages/2010/spr-2010/messages/msg-2-spr_10.html',
+		name: 'msg-2-spr_10',
+	},
+	{
+		path: '/msg-3-spr_10/',
+		url: './pages/2010/spr-2010/messages/msg-3-spr_10.html',
+		name: 'msg-3-spr_10',
+	},
+	{
+		path: '/msg-4-spr_10/',
+		url: './pages/2010/spr-2010/messages/msg-4-spr_10.html',
+		name: 'msg-4-spr_10',
+	},
+
+
+
 	/* Conference pages*/
 	{
 		path: '/season-2015/',
@@ -166,6 +329,21 @@ var routes = [
 		path: '/season-2013/',
 		url: './pages/2013/season-2013.html',
 		name: 'season-2013',
+	},
+	{
+		path: '/season-2012/',
+		url: './pages/2012/season-2012.html',
+		name: 'season-2012',
+	},
+	{
+		path: '/season-2011/',
+		url: './pages/2011/season-2011.html',
+		name: 'season-2011',
+	},
+	{
+		path: '/season-2010/',
+		url: './pages/2010/season-2010.html',
+		name: 'season-2010',
 	},
 		// Message pages
 	{
@@ -193,8 +371,36 @@ var routes = [
 		url: './pages/2013/fall-2013/msg-f-2013.html',
 		name: 'msg-f-2013',
 	},
-
-
+	{
+		path: '/msg-s-2012/',
+		url: './pages/2012/spr-2012/msg-s-2012.html',
+		name: 'msg-s-2012',
+	},
+	{
+		path: '/msg-f-2012/',
+		url: './pages/2012/fall-2012/msg-f-2012.html',
+		name: 'msg-f-2012',
+	},
+	{
+		path: '/msg-s-2011/',
+		url: './pages/2011/spr-2011/msg-s-2011.html',
+		name: 'msg-s-2011',
+	},
+	{
+		path: '/msg-f-2011/',
+		url: './pages/2011/fall-2011/msg-f-2011.html',
+		name: 'msg-f-2011',
+	},
+	{
+		path: '/msg-s-2010/',
+		url: './pages/2010/spr-2010/msg-s-2010.html',
+		name: 'msg-s-2010',
+	},
+	{
+		path: '/msg-f-2010/',
+		url: './pages/2010/fall-2010/msg-f-2010.html',
+		name: 'msg-f-2010',
+	},
 
 
 
